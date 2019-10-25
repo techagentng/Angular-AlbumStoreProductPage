107bdb9ff8d7be7c9ec66617e1995fcf3bc29789
Kenneth <techagentng@gmail.com>
2019-10-25 23:16:16 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,12 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
-import { ProductDescriptionComponentComponent } from './product-description-component/product-description-component.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductPageComponent,
-    ProductDescriptionComponentComponent
+    ProductPageComponent
   ],
   imports: [
     BrowserModule,
